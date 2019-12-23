1cd6c64f005bac1d4b02efc5a5974ed18088f43c
Matteo <mstievano9@gmail.com>
2019-12-23 18:54:05 +0100

    aggiunti commenti al file GFA_2.py

--- GFA_2.py
+++ GFA_2.py
@@ -1,1 +1,7 @@
-#validator per file GFA 2.0
+# validator per file GFA 2.0
+# questo programma dato un file in formato .gfa (o .txt) deve controllare se il file rispetta gli standard GFA 2.0
+# il formato GFA 2.0 è una generalizzazione del formato GFA 1.0, quindi questo validatore deve essere in grado di controllare
+# senza problemi entrambi i formati.
+
+# il validatore NON deve controllare se le istruzioni sono logicamente correte,
+# deve SOLO controllare che siano sintatticamente corrette 
