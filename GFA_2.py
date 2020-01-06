20d4f28c0dd26120438b401d5c5e2a3af8ccdb50
DrStiev <mstievano9@gmail.com>
2020-01-06 19:35:43 +0100

    aggiunta roba

--- GFA_2.py
+++ GFA_2.py
@@ -10,14 +10,12 @@ import os
 import re 
 import json
 
-#global variable per controllo errori
-LineErrorFlag = False
+
 dictionaryError = {}
+errorMex = ''
 
 def main():
     try:
-        # assegno alla variabile il nome del file che si vuole controllare 
-        # il nome del file e' letto da riga di comando
         # GFACompleteFilePath = sys.argv[1] # per ora non lo uso MA credo che sarebbe preferibile utilizzarlo poi
         GFACompleteFilePath = input('Inserire il path completo del file da voler controllare: ')
         # controllo che il path sia valido
@@ -28,50 +26,49 @@ def main():
         os._exit(1)
     
 def readGFAFileLines(GFAFilePath):
-    # funzione che legge tutto il file riga per riga
-    # in base al primo elemento della riga (che identifica che tipo di riga e') 
-    # reindirizza alla sottofunzione adatta la riga da che deve essere analizzata
-    # le sottofunzioni prendono in input una riga (con o senza tab bisogna vedere come e' comodo)
-    # e il numero di riga
-    # apro il file e lo leggo
     with open(GFAFilePath,'r') as GFAFileToCheck:
         lineToCheck = GFAFileToCheck.readline()
         linePointer = 1
         while lineToCheck:
+            LineErrorFlag = False
             #stampe di controllo utilizzando il formato json per rendere 
             # tutto piu' leggibile possibile
             print(json.dumps(lineToCheck, indent=4, sort_keys=False))
-            # rigaSenzaTab = re.split('\t+', rigaConTab)
-            rigaSenzaTab = re.split('\t+|\n+', lineToCheck)
+            rigaSenzaTab = re.split('\t', re.sub('\n','', lineToCheck))            
             print(json.dumps(rigaSenzaTab, indent=4, sort_keys=False))
-            headToken = rigaSenzaTab[0]
-            print(headToken)
-            # chiamo lo switcher che gestirà poi le sottochiamate a funzione
-            findFunction(headToken)
-            # errorHandling o circa
+            headToken = lineToCheck[0]
+            if headToken == 'H':
+                # la regex dovrebbe essere questa ma non funziona
+                regexH = r"H\t([A-Za-z0-9][A-Za-z0-9]:[ABHJZif]:[ -~]*)\w?\t([A-Za-z0-9][A-Za-z0-9]:[ABHJZif]:{-}[0-9]+(,{-}[0-9]+)\w*)\w?\t([A-Za-z0-9][A-Za-z0-9]:[ABHJZif]:[ -~]*)\w*"
+                matchH = re.search(regexH, lineToCheck)  
+                if not matchH:
+                    LineErrorFlag = True
+                    errorMex = 'Gli HEADER TAG non sono corretti!' 
+            elif headToken == 'S':
+                 # la regex dovrebbe essere questa ma non funziona
+                regexS = r"S\t[!-~]+\t{-}[0-9]+(*)|[!-~]+\t([A-Za-z0-9][A-Za-z0-9]:[ABHJZif]:[ -~]*)\w*"
+                matchS = re.search(regexS, lineToCheck)
+                if not matchS:
+                    LineErrorFlag = True
+                    errorMex = 'I SEGMENT TAG non sono corretti'
+            elif headToken == 'F':
+                pass
+            elif headToken == 'E':
+                pass
+            elif headToken == 'G':
+                pass
+            elif headToken == 'O' or headToken == 'U':
+                pass
+            else:
+                pass 
+            # gfaErrorHandling o circa
             if LineErrorFlag:
                 GFAFileError(dictionaryError, linePointer, lineToCheck, errorMex)
-                print(json.dumps(dictionaryError, indent=4, sort_keys=False))
+                LineErrorFlag = False
             lineToCheck = GFAFileToCheck.readline()
-            print(linePointer)
             linePointer += 1
-
-def findFunction(token):
-    # non esiste il costrutto switch in python quindi o si usa un if annidato (if... elif... elif.... else)
-    # oppure si una un dictionary mapping (questo)
-    # o in alternativa si può usare un dispatch method for classes
-    switcher = {
-        'H': HeaderFunction,
-        'S': SegmentFunction,
-        'F': FragmentFunction,
-        'E': EdgeFunction,
-        'G': GapFunction,
-        'O': GroupFunction,
-        'U': GroupFunction,
-    }
-    # ottengo la funzione relativa dallo switcher dictionary 
-    func = switcher.get(token)
-    return func()
+        print(json.dumps(dictionaryError, indent=4, sorted_keys=False))
+        print('Programma terminato correttamente')
 
 def GFAFileError(errorDictionary, NRiga, TestoRiga, MessaggioErrore):
     # si potrebbe pure creare una funzione che si occupa di accorpare le righe adiacenti di errore
