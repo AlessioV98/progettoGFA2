b5151188c864d43759fa3cbb7ead21d0e8eead8d
DrStiev <mstievano9@gmail.com>
2020-01-27 16:14:23 +0100

    prova

--- GFA_2_FILE_VALIDATOR.py
+++ GFA_2_FILE_VALIDATOR.py
@@ -51,6 +51,7 @@ def PrintInfo():
     risposta = input('Vuoi conoscere tutti i cambiamenti che sono '
                     + 'stati apportati da GFA 1.0 a GFA 2.0?' 
                     + '[Y/N]\n')
+    print(risposta)
     if risposta.upper() == 'Y':
         webbrowser.open('https://bit.ly/2FsZXrM')
         # indirizzamento (1) verso risorsa github 
